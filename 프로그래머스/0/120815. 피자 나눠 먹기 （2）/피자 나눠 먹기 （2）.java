b49759d9ede28894d3337ca9fee4046db5b3d20c
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-04-02 10:01:07 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int p = 1;
+        // 피자판 p
+        // 조각수 p*6
+        // 조각수 % n = 0 true
+        while((p*6) % n !=0){
+            if((p*6) % n ==0)
+                break;
+            p++;
+        }
+        return p;
+    }
+}
