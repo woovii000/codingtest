612727b9a9fbf84f80268cb4fe79eb04a692128d
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-04-04 19:05:24 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.53 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,10 +1,8 @@
-import java.util.Arrays; //import 필수
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array); // 오름차순정렬 - import 필요
-        int answer = 0;
-        answer = array[array.length/2]; // 정렬된 배열의 중앙값
-        return answer;
+        Arrays.sort(array);
+        return array[array.length/2];
     }
 }
