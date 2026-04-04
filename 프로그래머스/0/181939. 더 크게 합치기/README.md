0ebdc0391bc4ce0afcfb4a9e9df45bcd90fa0a63
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-04-04 20:19:54 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.87 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/README.md
+++ 프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 2.33 ms
+메모리: 90.6 MB, 시간: 1.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 02일 14:09:26
+2026년 04월 04일 20:19:52
 
 ### 문제 설명
 
--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
-        String stra = Integer.toString(a); //숫자 -> 글씨 변환
-        String strb = Integer.toString(b); 
-        String strab = ""; //empty
-        int answer = 0;
-        if(Integer.parseInt(stra+strb) >= Integer.parseInt(strb+stra)){
-            answer = Integer.parseInt(stra+strb); //글자 -> 숫자 변환
-        }else{
-            answer = Integer.parseInt(strb+stra);
-        }
-        return answer; //출력
+        String AB = String.valueOf(a)+String.valueOf(b);
+        String BA = String.valueOf(b)+String.valueOf(a);
+        if(Integer.parseInt(AB)>=Integer.parseInt(BA))
+            return Integer.parseInt(AB);
+        else
+            return Integer.parseInt(BA);
     }
 }
