43d072c111ccea47dcc62b1f40950fc8ab186951
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-09 23:56:24 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 9.55 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,23 +1,29 @@
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
     public int[] solution(String my_string) {
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i =0; i<my_string.length(); i++){
-            if('0' <= my_string.charAt(i) && my_string.charAt(i) <= '9'){ //숫자 골라내기
-                list.add((my_string.charAt(i)-'0')); //배열에 넣기 charAt 문자 -> ?? - '0' 하면 숫자값
-            }   
+
+        String nums = "";
+
+        // 숫자만 추출
+        for(int i = 0; i < my_string.length(); i++) {
+
+            if('0' <= my_string.charAt(i) && my_string.charAt(i) <= '9') {
+                nums += my_string.charAt(i);
+            }
         }
-        //list -> 배열
-        //배열 -> sort(타입 확인)
-        //int[] answer = list.toArray(new int[list.size()]); // 불가 list - Integer 객체*
-        int[] answer = new int[list.size()];
-        for(int i =0; i<list.size(); i++){
-            answer[i] = list.get(i);
+
+        // 문자열 길이만큼 배열 생성
+        int[] answer = new int[nums.length()];
+
+        // 문자 → 숫자 변환
+        for(int i = 0; i < nums.length(); i++) {
+            answer[i] = nums.charAt(i) - '0';
         }
+
+        // 오름차순 정렬
         Arrays.sort(answer);
+
         return answer;
     }
 }
