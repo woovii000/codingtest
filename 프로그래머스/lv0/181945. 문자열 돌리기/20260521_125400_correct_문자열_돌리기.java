1bf54dcf0151c94ba0fb2abd9cb8a8b18104e81f
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-21 12:54:02 +0900

    [ALG] correct | 프로그래머스 | lv0 | 문자열 돌리기 | Java | 9번째 시도

---
+++ 프로그래머스/lv0/181945. 문자열 돌리기/20260521_125400_correct_문자열_돌리기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new Scanner(System.in).next();
+        for(int i=0; i< a.length(); i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+}
