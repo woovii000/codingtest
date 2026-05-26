b588265f0611ec743f933a91007eb2b22e7af911
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-26 13:58:53 +0900

    [ALG] run | 프로그래머스 | lv0 | 홀짝에 따라 다른 값 반환하기 | Java | 3번째 시도

---
+++ 프로그래머스/lv0/181935. 홀짝에 따라 다른 값 반환하기/20260526_135851_run_홀짝에_따라_다른_값_반환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        if(n%2==0){
+            for(int i=0; i<=n; i+2){ sum += i; }
+        }else{
+            for(int i=1; i<=n; i+2){ sum += i; }
+        }
+        return sum;
+    }
+}
