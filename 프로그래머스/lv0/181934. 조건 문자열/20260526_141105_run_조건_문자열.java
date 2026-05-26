4b42767dc6b69d418ae560aa6bc471b332e54dd1
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-26 14:11:07 +0900

    [ALG] run | 프로그래머스 | lv0 | 조건 문자열 | Java | 7번째 시도

---
+++ 프로그래머스/lv0/181934. 조건 문자열/20260526_141105_run_조건_문자열.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        if(ineq==">" && eq=="="){return n>=m ? 1:0;
+        }else if(ineq=="<" && eq=="<"){return n<=m ? 1:0;
+        }else if(ineq==">" && eq=="!"){return n>m ? 1:0;
+        }else if(ineq=="<" && eq=="!"){return n<m ? 1:0;}
+    }
+}
