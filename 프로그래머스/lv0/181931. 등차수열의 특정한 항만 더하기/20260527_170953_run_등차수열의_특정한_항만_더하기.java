a77a64beea6e8b00bfae1714ba72465adb7509de
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-27 17:09:55 +0900

    [ALG] run | 프로그래머스 | lv0 | 등차수열의 특정한 항만 더하기 | Java | 7번째 시도

---
+++ 프로그래머스/lv0/181931. 등차수열의 특정한 항만 더하기/20260527_170953_run_등차수열의_특정한_항만_더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int sum = 0;
+        for(int i=0; i<boolean.length; i++){
+            if(boolean[i]){
+                sum += a+i*d;
+            }
+        }
+        return sum;
+    }
+}
