73ab6abf7c53c1323e640cc2f53919cdc74b912b
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-27 16:03:05 +0900

    [ALG] correct | 프로그래머스 | lv0 | 코드 처리하기 | Java | 18번째 시도

---
+++ 프로그래머스/lv0/181932. 코드 처리하기/20260527_160304_correct_코드_처리하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i=0; i<code.length(); i++){
+            if(code.charAt(i) == '1'){
+                mode = mode==0 ? 1:0;
+            }else{
+                if(mode==0 && i%2==0){
+                    answer += code.charAt(i);
+                }else if(mode==1 && i%2==1){
+                    answer += code.charAt(i);
+                }
+            }
+        }
+        return answer;
+    }
+}
