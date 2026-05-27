76920263d0ba61fad1929de5eed238cb3a04c791
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-27 17:28:53 +0900

    [ALG] run | 프로그래머스 | lv0 | 주사위 게임 2 | Python3 | 4번째 시도

---
+++ 프로그래머스/lv0/181930. 주사위 게임 2/20260527_172852_run_주사위_게임_2.py
@@ -0,0 +1,9 @@
+def solution(a, b, c):
+    if a!=b and b!=c and c!=a:
+        return a+b+c
+    elif a==b and b==c and c==a:
+        return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    elif a==b or b==c or c==a:
+        return (a+b+c)*(a**2+b**2+c**2)
+    else:
+        return 0
