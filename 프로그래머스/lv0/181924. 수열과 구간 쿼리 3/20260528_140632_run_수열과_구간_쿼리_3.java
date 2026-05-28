412576c91493bfd383e577e312420f6ee8ceae9c
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-28 14:06:34 +0900

    [ALG] run | 프로그래머스 | lv0 | 수열과 구간 쿼리 3 | Java | 3번째 시도

---
+++ 프로그래머스/lv0/181924. 수열과 구간 쿼리 3/20260528_140632_run_수열과_구간_쿼리_3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            int temp = 0;
+            temp = answer[queries[i][0]];
+            answer[queries[i][0]] = answer[queries[i][1]];
+            answer[queries[i][1]] = temp;
+        }
+        return answer;
+    }
+}
