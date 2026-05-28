f19fbcf8f5f7a0f1dd1bbc06a64907cd257abccf
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-28 11:08:40 +0900

    [ALG] correct | 프로그래머스 | lv0 | 수 조작하기 1 | Python3 | 6번째 시도

---
+++ 프로그래머스/lv0/181926. 수 조작하기 1/20260528_110839_correct_수_조작하기_1.py
@@ -0,0 +1,9 @@
+def solution(n, control):
+    for ch in control:
+        if ch == "w": n+=1
+        elif ch == "s": n-=1
+        elif ch == "d": n+=10
+        else: n-=10
+    return n
+            
+        
