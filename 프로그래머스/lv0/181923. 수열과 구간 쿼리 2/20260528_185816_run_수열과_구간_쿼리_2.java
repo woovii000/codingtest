bc2e8acc893472886dde01f20b987e9ed3ef75a1
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-28 18:58:18 +0900

    [ALG] run | 프로그래머스 | lv0 | 수열과 구간 쿼리 2 | Java | 17번째 시도

---
+++ 프로그래머스/lv0/181923. 수열과 구간 쿼리 2/20260528_185816_run_수열과_구간_쿼리_2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        int min_value = queries[i][2]+1;
+        for(int i=0; i<queries.length; i++){
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+               if(queries[i][2]<arr[j])
+                   min_value = Math.min(min_value,arr[j]);
+            }
+            if(min_value != queries[i][2]){ answer[i] = min_value; }
+            else{ answer[i] = -1; }   
+        }
+        return answer;
+    }
+}
