700a29aef302175e8cf5108135cace12c6f3094e
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-28 12:10:49 +0900

    [ALG] correct | 프로그래머스 | lv0 | 수 조작하기 2 | Python3 | 3번째 시도

---
+++ 프로그래머스/lv0/181925. 수 조작하기 2/20260528_121048_correct_수_조작하기_2.py
@@ -0,0 +1,12 @@
+def solution(numLog):
+    answer = []
+    for i in range(len(numLog)-1):
+        if numLog[i+1]-numLog[i]==1:
+            answer.append('w')
+        elif numLog[i+1]-numLog[i]==-1:
+            answer.append('s')
+        elif numLog[i+1]-numLog[i]==10:
+            answer.append('d')
+        else:
+            answer.append('a')
+    return "".join(answer)
