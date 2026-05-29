4047e9dc85e958aab296ceb3865174be5dc7cee4
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-29 18:08:20 +0900

    [ALG] run | 프로그래머스 | lv0 | 배열 만들기 4 | Python3 | 6번째 시도

---
+++ 프로그래머스/lv0/181918. 배열 만들기 4/20260529_180818_run_배열_만들기_4.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    stk = []
+    for i in range(len(arr)):
+        if stk:
+            while stk[-1]>=arr[i]:
+                stk.pop()
+            stk.append(arr[i])
+        else:
+            stk.append(arr[i])
+    return stk
