9cdd22bbeee250e57e04dc9bb8963851e992106a
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-29 10:02:45 +0900

    [ALG] run | 프로그래머스 | lv0 | 수열과 구간 쿼리 4 | Python3 | 5번째 시도

---
+++ 프로그래머스/lv0/181922. 수열과 구간 쿼리 4/20260529_100244_run_수열과_구간_쿼리_4.py
@@ -0,0 +1,6 @@
+def solution(arr, queries):
+    answer = arr[:]
+    for s,e,k in queries:
+        for i in range(s,e+1):
+            answer[i] = answer[i]+1 if arr[i] !=0 and arr[i]%k==0 else answer[i]
+    return arr
