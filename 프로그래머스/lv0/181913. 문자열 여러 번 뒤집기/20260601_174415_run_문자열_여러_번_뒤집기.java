14214a09e13fac91b09c52d7f6b57906eba680eb
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-01 17:44:16 +0900

    [ALG] run | 프로그래머스 | lv0 | 문자열 여러 번 뒤집기 | Java | 17번째 시도

---
+++ 프로그래머스/lv0/181913. 문자열 여러 번 뒤집기/20260601_174415_run_문자열_여러_번_뒤집기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        String[] answer = new String[my_string.length()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = my_string.valueOf(i);
+        }
+        for(int i=0; i<queries.length; i++){
+            for(int j=0; j<(queries[i][1]-queries[i][0]+1)/2; j++){
+                char c = '';
+                c = answer[queries[i][0]+i]; 
+                answer[queries[i][0]+i] = answer[queries[i][1]-i];
+                answer[queries[i][1]-i] = temp;
+            }
+        }
+        return Arrays.toString(answer);
+    }
+}
