a5c60e7eb9e964a0da932da777f5d44e9cae2c31
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-01 19:03:35 +0900

    [ALG] run | 프로그래머스 | lv0 | 주사위 게임 3 | Python3 | 6번째 시도

---
+++ 프로그래머스/lv0/181916. 주사위 게임 3/20260601_190334_run_주사위_게임_3.py
@@ -0,0 +1,32 @@
+# set(), count()
+def solution(a, b, c, d):
+    arr = [a,b,c,d]
+    # 모두 같을 때
+    if len(set(arr)) == 1:
+        return 1111*arr[0]
+    elif len(set(arr)) == 2:
+        p, q = 0, 0
+        for i in set(arr):
+            # 세 개가 같을 때
+            if arr.count(i) == 3:
+                p = i
+            elif arr.count(i) == 1:
+                q = i
+        return (10*p+q)**2
+                
+        # 2개*2세트가 같을 때
+        else:
+            nums = list(set(arr))
+            p = nums[0]
+            q = nums[1]
+            return (p+q)*abs(p-q)
+    # 2같 1,1 다름
+    elif len(set(arr)) == 3:
+        others = []
+        for i in set(arr):
+            if arr.count(i) !=2:
+                others.append(i)
+        return others[0] * others[1]
+    # 모두 다를 때
+    else:
+        return min(arr)
