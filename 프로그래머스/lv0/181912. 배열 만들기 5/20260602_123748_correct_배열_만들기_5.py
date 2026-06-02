92e66940b77b6ba5a26aef8ede9845a93d5f6dae
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-02 12:37:49 +0900

    [ALG] correct | 프로그래머스 | lv0 | 배열 만들기 5 | Python3 | 3번째 시도

---
+++ 프로그래머스/lv0/181912. 배열 만들기 5/20260602_123748_correct_배열_만들기_5.py
@@ -0,0 +1,7 @@
+def solution(intStrs, k, s, l):
+    # 슬라이스 s:s+l -> int(슬라이스) > k -> append.
+    answer = []
+    for str in intStrs:
+        if int(str[s:s+l]) > k:
+            answer.append(int(str[s:s+l]))
+    return answer
