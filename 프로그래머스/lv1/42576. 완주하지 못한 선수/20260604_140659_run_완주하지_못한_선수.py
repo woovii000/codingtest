d45e3f37964c7b476f19e9934e33a55cc39c93fe
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-04 14:07:01 +0900

    [ALG] run | 프로그래머스 | lv1 | 완주하지 못한 선수 | Python3 | 9번째 시도

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/20260604_140659_run_완주하지_못한_선수.py
@@ -0,0 +1,14 @@
+def solution(participant, completion):
+    # 안 뛴 사람 1명 찾아내기**
+    # 인원 많음, 동명이인 있을 수 있음**
+    # 1) 이중 for 동일하면 pop -> 남은 인원 있는 배열 출력 
+    # => 배열크기 변경됨, O(n²) | 실패
+    # 2) 리스트 복사 후, 있다면, 복사한 list에서 pop*
+    # => remove(value)(o), pop(index)(x), O(n²) | 효율성 실패 
+    # 3) 정렬 후, 비교 => sort(). i ~ 순차 비교, O(n)
+    # 4) Hash
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
