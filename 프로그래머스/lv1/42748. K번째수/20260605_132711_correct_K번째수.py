f90d4b90430f847a21eadf41c9d07cd74a383ef9
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-05 13:27:12 +0900

    [ALG] correct | 프로그래머스 | lv1 | K번째수 | Python3 | 11번째 시도

---
+++ 프로그래머스/lv1/42748. K번째수/20260605_132711_correct_K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for i, j, k in commands:
+        slide = array[i-1:j]
+        slide.sort()
+        answer.append(slide[k-1])
+    return answer
