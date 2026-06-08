a30b5ae2917988cea43d8b3ea983cb634962e7bb
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-08 09:04:56 +0900

    [ALG] run | 프로그래머스 | lv1 | 모의고사 | Python3 | 2번째 시도

---
+++ 프로그래머스/lv1/42840. 모의고사/20260608_090454_run_모의고사.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    # 정답 패턴
+    pattern = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    # 정답 수
+    check = [0,0,0]
+    answer = []
+    # 정답 체크 -> 정답? +1
+    for i in range(len(answers)):
+        if pattern[0][i%5] == answers[i]:
+            answer[0] += 1
+        if pattern[1][i%8] == answers[i]:
+            answer[1] += 1
+        if pattern[2][i%10] == answers[i]:
+            answer[2] += 1   
+    for i in range(len(check)):
+        if check[i] == max(check):
+            answer.append(i+1)
+    return answer
