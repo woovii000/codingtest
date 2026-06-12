c947b1e801adbab34b236ab1a8cd0db2fcd14bb3
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-12 12:51:27 +0900

    [ALG] correct | 프로그래머스 | lv1 | 체육복 | Python3 | 11번째 시도

---
+++ 프로그래머스/lv1/42862. 체육복/20260612_125127_correct_체육복.py
@@ -0,0 +1,20 @@
+def solution(n, lost, reserve):
+    # lost.value == reserve.value => 제외(pop)
+    # lost.value+-1 == reserve.value => 제외(pop)
+    # n - len(lost)
+    lost.sort()
+    reserve.sort()
+    # 도난 + 여벌 유저
+    for x in lost[:]:
+        if x in reserve[:]:   
+            lost.remove(x)
+            reserve.remove(x)
+    # 여벌옷 빌려주기
+    for x in lost[:]:
+        if x-1 in reserve[:]:
+            lost.remove(x)
+            reserve.remove(x-1)
+        elif x+1 in reserve[:]:
+            lost.remove(x)
+            reserve.remove(x+1)
+    return n-len(lost)
