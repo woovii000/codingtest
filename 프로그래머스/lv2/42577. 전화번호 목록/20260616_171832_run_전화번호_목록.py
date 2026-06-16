33538177f4a82e122845ba10a06708a0c85e999c
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-16 17:18:34 +0900

    [ALG] run | 프로그래머스 | lv2 | 전화번호 목록 | Python3 | 17번째 시도

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/20260616_171832_run_전화번호_목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    # phone_book의 원소 중에 번호 겹침o -> False
+    # 겹침x -> True
+    # 포인트: 접두어* -> startswith()
+    # 이중 for → O(n²) => 오답
+    # 정렬 후, 비교: 정렬 → O(n log n)
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i].startswith(phone_book[i+1]):
+            return False
+    return True
