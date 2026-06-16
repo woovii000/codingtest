e768700b386075187fc814b5d8d00fa30baf669a
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-16 17:09:41 +0900

    [ALG] run | 프로그래머스 | lv2 | 전화번호 목록 | Python3 | 6번째 시도

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/20260616_170940_run_전화번호_목록.py
@@ -0,0 +1,6 @@
+def solution(phone_book):
+    # phone_book[0] -> phone_book[1], phone_book[2] 중에 겹치면 False 아님 True
+    for i in range(1, len(phone_book)):
+        if phone_book[0] in phone_book[i]:
+            return False
+    return True
